75ff587669cabcaaf32213a887a97008cc13efee
Michael Markowski <mic@ono-sendai.fritz.box>
2010-07-01 01:08:27 +0200

    v.0.6.1, see CHANGES

---
+++ AFURLCache.m
@@ -0,0 +1,60 @@
+/*
+ *
+ * Copyright 2008 Artifacts - Fine Software Development
+ * http://www.artifacts.de
+ * Contributed by Nico Schmidt - savoysoftware.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#import "AFURLCache.h"
+#import "AFCache.h"
+
+@implementation AFURLCache
+
+-(NSCachedURLResponse*)cachedResponseForRequest:(NSURLRequest*)request
+{
+    NSURL* url = request.URL;
+    if (![[AFCache sharedInstance] hasCachedItemForURL:url])
+    {
+//		NSLog(@"cachedResponseForRequest: %@", [request description]);
+		NSCachedURLResponse *response = [super cachedResponseForRequest:request];
+        return response;
+    }
+    
+    AFCacheableItem* item = [[AFCache sharedInstance] cachedObjectForURL:url options:0];
+    if (nil == item)
+    {
+        return nil;
+    }
+    
+    NSData* data = item.data;
+    NSURLResponse* response = [[NSURLResponse alloc] initWithURL:url 
+														MIMEType:item.mimeType 
+										   expectedContentLength:[data length] textEncodingName:nil];
+
+    return [[NSCachedURLResponse alloc] initWithResponse:response data:data];
+}
+
+- (void)storeCachedResponse:(NSCachedURLResponse *)cachedResponse forRequest:(NSURLRequest *)request
+{
+	// TODO: put response into cache
+	// find out how implicit requests initiated by html references (e.g. <img>) could be cached.
+	// Although cachedResponseForRequest: is called, storeCachedResponse:forRequest: is not called
+	// for implicit requests.
+	
+//	NSLog(@"request %@ resulted in response: %@", [request description], [cachedResponse description]);
+}
+
+@end
