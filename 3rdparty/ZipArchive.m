66fb33eb7762f73fd070654cd820f2c3598a2fa6
Nico Schmidt <nico@savoysoftware.com>
2010-08-02 21:50:08 +0200

    Fixed some minor issues

--- 3rdparty/ZipArchive.m
+++ 3rdparty/ZipArchive.m
@@ -146,7 +146,7 @@
 		unz_global_info  globalInfo = {0};
 		if( unzGetGlobalInfo(_unzFile, &globalInfo )==UNZ_OK )
 		{
-			NSLog([NSString stringWithFormat:@"%d entries in the zip file",globalInfo.number_entry] );
+			NSLog(@"%@", [NSString stringWithFormat:@"%d entries in the zip file",globalInfo.number_entry] );
 		}
 	}
 	return _unzFile!=NULL;
@@ -196,7 +196,7 @@
 		filename[fileInfo.size_filename] = '\0';
 		
 		// check if it contains directory
-		NSString * strPath = [NSString  stringWithCString:filename];
+		NSString * strPath = [[NSFileManager defaultManager] stringWithFileSystemRepresentation:filename length:fileInfo.size_filename];
 		BOOL isDirectory = NO;
 		if( filename[fileInfo.size_filename-1]=='/' || filename[fileInfo.size_filename-1]=='\\')
 			isDirectory = YES;
