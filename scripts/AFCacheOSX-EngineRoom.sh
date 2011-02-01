273a1b4745d769201af94892cd2679d74e18e1a3
Bjoern Kriews <bkr@jumper.org>
2011-02-01 17:16:46 +0100

    added AFCacheOSX-EngineRoom target for use in parent-projects

---
+++ scripts/AFCacheOSX-EngineRoom.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+#set -x
+
+OSX_SDK=macosx10.6
+OSX_ARCHS="x86_64 i386 ppc"
+
+
+ENGINEROOM_CFLAGS="-DDONT_USE_ENGINEROOM"
+ENGINEROOM_LDFLAGS=""
+
+ENGINEROOM_OSX="$SOURCE_ROOT/../EngineRoom/EngineRoom-OSX"
+
+if [ -e "$ENGINEROOM_OSX" ] ; then
+	ENGINEROOM_CFLAGS="-DUSE_ENGINEROOM -DER_EMBEDDED_NAME=AFCache"
+	ENGINEROOM_LDFLAGS="-weak_framework EngineRoom"
+
+	pushd "$ENGINEROOM_OSX"
+	xcodebuild -target EngineRoom-OSX -sdk $OSX_SDK -configuration "$CONFIGURATION" \
+		"BUILD_DIR=$BUILD_DIR" \
+		"ARCHS=$OSX_ARCHS" \
+		 clean build
+	popd
+fi
+
+XCCONFIG="$BUILD_DIR/AFCache.xcconfig"
+cat <<__EOCONFIG__ > "$XCCONFIG"
+ARCHS=$OSX_ARCHS
+ENGINEROOM_CFLAGS=$ENGINEROOM_CFLAGS
+ENGINEROOM_LDFLAGS=$ENGINEROOM_LDFLAGS
+__EOCONFIG__
+
+xcodebuild -target AFCacheOSX -sdk $OSX_SDK -configuration "$CONFIGURATION" -xcconfig "$XCCONFIG" clean build
+
+exit 0
