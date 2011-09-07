2653a4dbdf04dad45ec50dc8cb7c7287b4c60794
Brandon Sneed <brandon@redf.net>
2011-09-07 16:38:46 -0700

    fixed build script.

--- scripts/createFatLib.sh
+++ scripts/createFatLib.sh
@@ -5,9 +5,11 @@ set -e
 set -x
 
 
-xcodebuild -target AFCache -sdk iphoneos4.3 "ARCHS=armv6 armv7" clean build
-xcodebuild -target AFCache -sdk iphonesimulator4.3 "ARCHS=i386 x86_64" "VALID_ARCHS=i386 x86_64" clean build
-lipo -output release/libAFCache.a -create $XCODE_BUILD_DIR/Release-iphoneos/libAFCache.a $XCODE_BUILD_DIR/Release-iphonesimulator/libAFCache.a
+xcodebuild -project AFCache-iOS.xcodeproj -target AFCache-iOS -sdk iphoneos4.3 "ARCHS=armv6 armv7" clean build
+xcodebuild -project AFCache-iOS.xcodeproj -target AFCache-iOS -sdk iphonesimulator4.3 "ARCHS=i386 x86_64" "VALID_ARCHS=i386 x86_64" clean build
+lipo -output release/libAFCache-iOS.a -create $XCODE_BUILD_DIR/Release-iphoneos/libAFCache-iOS.a $XCODE_BUILD_DIR/Release-iphonesimulator/libAFCache-iOS.a
 
+cd scripts
 ./updateAPI.sh
+cd ..
 cp CHANGES release/
