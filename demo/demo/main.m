a99a5006404e79464d43d540d2740528c26353b4
Claus Weymann <claus.weymann@sprylab.com>
2014-03-12 17:21:41 +0100

    https://redmine.purplemotion.com/issues/1429 - redirects can be injected now, demo project added that is pod based - refs #1429

---
+++ demo/demo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  demo
+//
+//  Created by Claus Weymann on 12.03.14.
+//  Copyright (c) 2014 purplemotion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AFAppDelegate class]));
+	}
+}
