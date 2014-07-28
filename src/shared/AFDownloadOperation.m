4ce197b1429b8f110677f5b374d6ab95e4d06070
Sebastian Grimme <sebastian.grimme@sprylab.com>
2014-07-28 10:38:46 +0200

    added download operation, start implementing Downloads as NSOperations

---
+++ src/shared/AFDownloadOperation.m
@@ -0,0 +1,13 @@
+//
+//  AFDownloadOperation.m
+//  AFCache
+//
+//  Created by Sebastian Grimme on 28.07.14.
+//  Copyright (c) 2014 Artifacts - Fine Software Development. All rights reserved.
+//
+
+#import "AFDownloadOperation.h"
+
+@implementation AFDownloadOperation
+
+@end
